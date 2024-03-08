e06277bab94bade28724f344fbc9acf711723800
Christian <95107072+christiankriedemann@users.noreply.github.com>
2024-03-08 16:11:33 +0100

    Version 1.31.2

--- build_image.sh
+++ build_image.sh
@@ -87,11 +87,12 @@ if [ $? -ne 0 ]; then
 else
     echo "Image wurde erfolgreich gebaut."
     # Lösche das lokale Image
-    docker rmi ${DOCKER_USERNAME}/${IMAGE_NAME}:${NEW_VERSION}
-    docker rmi ${DOCKER_USERNAME}/${IMAGE_NAME}:latest
-    # Committe die Versionsänderung
+    # docker rmi ${DOCKER_USERNAME}/${IMAGE_NAME}:${NEW_VERSION}
+    # docker rmi ${DOCKER_USERNAME}/${IMAGE_NAME}:latest
+    # Git add, commit und push für die Versionsänderung
     git add .
     git commit -m "Version $NEW_VERSION"
+    git push
 fi
 
 echo "Alle Operationen erfolgreich ausgeführt."
